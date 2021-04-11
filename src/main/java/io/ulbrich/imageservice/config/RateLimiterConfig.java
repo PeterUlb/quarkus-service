a27ac1230349a061ba57fd92fc33d05ed449fbb6
PeterUlb <peter@ulbrich.io>
2021-04-11 21:33:49 +0200

    Add initial Dev docker compose

--- src/main/java/io/ulbrich/imageservice/config/RateLimiterConfig.java
+++ src/main/java/io/ulbrich/imageservice/config/RateLimiterConfig.java
@@ -1,15 +1,16 @@
 package io.ulbrich.imageservice.config;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.util.Map;
 
 @ApplicationScoped
 public class RateLimiterConfig {
-    private final long defaultRate;
+    private long defaultRate;
     private Map<Long, Long> endpointGroupRates;
 
-    public RateLimiterConfig() {
-        System.out.println("created :)");
+    @PostConstruct
+    void init() {
         this.endpointGroupRates = Map.of(
                 0L, 100L,
                 1L, 100L,
